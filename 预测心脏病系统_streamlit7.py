19b5047a30efc15334d28d28e06f2dadab7be463
lpzpython <lpz_python@163.com>
2025-05-31 14:15:25 +0800

    Update 预测心脏病系统_streamlit7.py

--- 预测心脏病系统_streamlit7.py
+++ 预测心脏病系统_streamlit7.py
@@ -114,7 +114,7 @@ def load_and_clean_data():
 def train_model(df):
     X = df[['age', 'sex', 'trestbps', 'chol', 'fbs', 'thalach', 'exang', 'thal']]
     y = df['target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     param_grid = {
         'n_estimators': [100, 50],
